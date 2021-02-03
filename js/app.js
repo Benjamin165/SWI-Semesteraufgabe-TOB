aba7ad65dcb0410b6ac7c6a3757e8d0720426dc1
Benjamin165 <52745596+Benjamin165@users.noreply.github.com>
2021-02-03 20:21:20 +0100

    yes

--- js/app.js
+++ js/app.js
@@ -1,26 +1,114 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
+document.addEventListener('DOMContentLoaded', getRooms, false);
+var eurRate = 1;
+startDate = new Date;
+endDate = new Date;
 
 function getRooms() {
-    return fetch('https://matthiasbaldauf.com/swi1hs20/rooms')
+    fetch('https://matthiasbaldauf.com/swi1hs20/rooms')
         .then((response) => {
             return response.json()
         })
-        .then(json => {
-            return json
+        .then(data => {
+          append_json(data);
         })
         .catch((err) => {
             console.log('Fetch Error :-S', err)
         });
+}
+
+function calculateEur(){
+  fetch('https://api.exchangeratesapi.io/latest?base=CHF')
+        .then((response) => {
+            return response.json()
+        })
+        .then(data => {          
+          console.log(data.rates.EUR);
+          eurRate = data.rates.EUR;
+          return data.rates.EUR;
+        })
+        .catch((err) => {
+            console.log('Fetch Error :-S', err)
+        });
+}
+
+function updateTable(){
+  var table = document.getElementById('roomsTable');
+  var i = 1;
+  fetch('https://matthiasbaldauf.com/swi1hs20/rooms')
+        .then((response) => {
+            return response.json()
+        })
+        .then(data => {
+          data.forEach(function(object) {
+            table.rows[i].innerHTML = '<td>' + isAvailable(object.available) + '</td>' +
+            '<td class="big">' + object.id + '</td>' +
+            '<td>' + object.name + '</td>' +
+            '<td class="big">' + object.address + '</td>' + 
+            '<td>' + correctCurrency(object.price) + '</td>' +
+            '<td class="big">' + object.maxpersons + '</td>' +
+            '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#reservierungModal" data-whatever="' + object.id + '">Reservierungen</button></td>';
+            i++;
+          })
+        })
+        .catch((err) => {
+            console.log('Fetch Error :-S', err)
+        });
+}
+
+function append_json(data){
+  var table = document.getElementById('roomsTable');
+  data.forEach(function(object) {
+    var tr = document.createElement('tr');
+    tr.innerHTML = '<td>' + isAvailable(object.available) + '</td>' +
+    '<td class="big">' + object.id + '</td>' +
+    '<td>' + object.name + '</td>' +
+    '<td class="big">' + object.address + '</td>' + 
+    '<td>' + correctCurrency(object.price) + '</td>' +
+    '<td class="big">' + object.maxpersons + '</td>' +
+    '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#reservierungModal" data-whatever="' + object.id + '">Reservierungen</button></td>';
+    table.appendChild(tr);
+  })
+}
+
+
+$(function() {
+
+  $('input[name="datefilter"]').daterangepicker({
+      autoUpdateInput: false,
+      locale: {
+          cancelLabel: 'Clear'
+      }
+  });
+
+  $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+      console.log(picker.startDate._d);
+      console.log(picker.endDate._d);
+      startDate = picker.startDate._d;
+      endDate = picker.endDate._d;
+      console.log(startDate.toUTCString());
+      console.log(endDate.toUTCString());
+  });
+
+  $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+  });
+
+});
+
+function isAvailable(ava){
+  if(ava == 1){
+    return '✔️'
+  } else {
+    return '❌'
+  }
+}
+
+function correctCurrency(x){
+  if (document.getElementById('togglecur').checked){
+    return x + ' CHF/h';
+  } else {
+    calculateEur();
+    return (x * eurRate).toFixed(1) + ' EUR/h';
+  }
 }
